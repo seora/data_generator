39c72362a42e4e9d789a55ca83c4ae2e65f8c115
seora <tjfk4938@hanmail.net>
2020-11-16 15:55:11 +0900

    switchpage

--- MRC/main.py
+++ MRC/main.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import re
 import sys
 import xlrd
 from PyQt5 import QtCore, QtGui
@@ -22,7 +23,7 @@ class MainWindow(Ui_MainWindow):
     global totalPage;      totalPage = 0
     global PageIdxlist;    PageIdxlist = []
     global idx; idx=0
-    global lenDatalist;     lenDatalist = 0
+    global lastdata;     lastdata = 0
 
     def __init__(self, mw):
         Ui_MainWindow.__init__(self)
@@ -37,6 +38,7 @@ class MainWindow(Ui_MainWindow):
 
         self.prevBtn.clicked.connect(self.onPrevPage)
         self.nextBtn.clicked.connect(self.onNextPage)
+        self.switchBtn.clicked.connect(self.onSwitchPage)
 
 
     def loadData(self):
@@ -66,7 +68,6 @@ class MainWindow(Ui_MainWindow):
             popup.setText("파일을 선택해주세요")
             popup.exec_()
 
-
     def saveData(self):
         popup = QMessageBox()
         popup.setStandardButtons(QMessageBox.Ok)
@@ -76,7 +77,6 @@ class MainWindow(Ui_MainWindow):
             try:
                 with open(filename, "w", encoding='utf-8') as csv_file:
                     writer = csv.writer(csv_file)
-
 
                     for row in datalist:
                         writer.writerrow(row)
@@ -154,7 +154,7 @@ class MainWindow(Ui_MainWindow):
         global my_file
         global totalPage
         global currentPage
-        global lenDatalist
+        global lastdata
 
         for row_data in my_file:
             row_list = []
@@ -167,7 +167,7 @@ class MainWindow(Ui_MainWindow):
 
             datalist.append(row_list)
 
-        lenDatalist = len(datalist)
+        lastdata = len(datalist)
 
         col_headers = ['SPEAKER', 'SENTENCE', 'DATAID', 'DOMAINID', 'DOMAIN', 'CATEGORY', 'SPEAKERID', 'SENTENCEID',
                        'MAIN', 'SUB', 'QA', 'QACNCT']
@@ -178,17 +178,17 @@ class MainWindow(Ui_MainWindow):
                 totalPage += 1
                 PageIdxlist.append(datalist.index(a))
 
-        PageIdxlist.append(lenDatalist)
+        PageIdxlist.append(lastdata)
 
         self.max_page.setText(str(totalPage))
 
         print("대화 첫번째항 리스트", PageIdxlist)
+        self.currentPage.setText("1")
         self.updateStatus()
 
 
     def onPrevPage(self):
         global idx
-        idx -= 1
 
         popup = QMessageBox()
         popup.setStandardButtons(QMessageBox.Ok)
@@ -196,27 +196,29 @@ class MainWindow(Ui_MainWindow):
         if idx <= 0 :
             popup.setText("이전 페이지가 없습니다")
             popup.exec_()
-            self.prevBtn.setEnabled(False)
 
         else:
+            idx -= 1
+            print("이전 페이지 Index :", idx)
+            self.currentPage.setText(str(idx))
             self.updateStatus()
             print(idx)
 
 
     def onNextPage(self):
         global idx
-        global lenDatalist
-
-        idx += 1
-        print("next누르고 난 후, ", idx)
+        global lastdata
 
         popup = QMessageBox()
         popup.setStandardButtons(QMessageBox.Ok)
 
-        if PageIdxlist[idx] == lenDatalist:
+        if PageIdxlist[idx] == lastdata:
             popup.setText("다음 페이지가 없습니다")
             popup.exec_()
         else:
+            idx += 1
+            self.currentPage.setText(str(idx + 1))
+            print("다음 페이지, Index는 ", idx)
             self.updateStatus()
             print(idx)
 
@@ -225,16 +227,48 @@ class MainWindow(Ui_MainWindow):
         global PageIdxlist
         global idx
 
-        self.data_table.clearContents()
-        self.currentPage.setText(str(idx+1))
+        limitIdx1 = PageIdxlist[idx]
+        limitIdx2 = PageIdxlist[idx+1]
 
-        for row_data in datalist[PageIdxlist[idx]:PageIdxlist[idx+1]]:
-            row = self.data_table.rowCount()
+        self.data_table.clearContents()
+
+        print("넘어온 Index : ", idx)
+        print("page index 경계값 : ", limitIdx1, limitIdx2)
+
+        for row_data in datalist[ limitIdx1 : limitIdx2 ]:
+            self.data_table.setRowCount(limitIdx2 - limitIdx1)
+            row = datalist.index(row_data) - limitIdx1
             print(row)
             self.data_table.insertRow(row)
             for column, stuff in enumerate(row_data):
                 item = QTableWidgetItem(stuff)
-                self.data_table.setItem(row - PageIdxlist[idx] + 1, column, item)
+                if idx == 0:
+                    self.data_table.setItem(row, column, item)
+                else:
+                    self.data_table.setItem(row, column, item)
+
+
+    def onSwitchPage(self):
+        global idx
+        global totalPage
+
+        sp = self.pageEdit.text()
+        pattern = re.compile('^[0-9]+$')
+        match = pattern.match(sp)
+        if not match:
+            QMessageBox.information(self, "Tips", "please enter a number.")
+            return
+        if sp == "":
+            QMessageBox.information(self, "Tips", "Please enter a jump page.")
+            return
+
+        idx = int(sp) - 1
+        if idx > totalPage or idx < 0:
+            QMessageBox.information(self, "Tips", "No page specified, re-enter.")
+            return
+
+        self.currentPage.setText(str(idx + 1))
+        self.updateStatus()
 
 
     def CreateData(self):
